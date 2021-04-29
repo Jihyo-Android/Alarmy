ee22872952ba4fd7f944c9bd21bc14d113bbd51f
Jihyo Kim <wlgy33@naver.com>
2021-04-29 23:57:20 +0900

    Alarm Item들을 담을 RecyclerView 준비
    Alarm Item을 담을 RecyclerView를 구현하기 위해 Adapter 작성중
    Alarm Item View인 alarm_item.xml을 layout에 생성

--- app/src/main/java/com/jhkim/alarmy/AlarmRecyclerViewActivity.kt
+++ app/src/main/java/com/jhkim/alarmy/AlarmRecyclerViewActivity.kt
@@ -2,7 +2,10 @@ package com.jhkim.alarmy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_alarm_recycler_view.*
 
 class AlarmRecyclerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,12 +13,26 @@ class AlarmRecyclerViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_alarm_recycler_view)
 
 
-
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-
-            }
-        }
     }
 
 }
+
+//class AlarmAdapter(
+//
+//) : RecyclerView.Adapter<AlarmAdapter.AlarmViewHolder>() {
+//
+//    override fun onCreateViewHolder(
+//        parent: ViewGroup,
+//        viewType: Int
+//    ): AlarmAdapter.AlarmViewHolder {
+//
+//    }
+//
+//    override fun getItemCount(): Int {
+//
+//    }
+//
+//    override fun onBindViewHolder(holder: AlarmAdapter.AlarmViewHolder, position: Int) {
+//
+//    }
+//}
