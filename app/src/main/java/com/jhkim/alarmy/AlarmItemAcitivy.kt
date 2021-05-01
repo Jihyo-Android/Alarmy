b9bf0fbd559c72b4adca7bb9e8f91d33bb446c45
Jihyo Kim <wlgy33@naver.com>
2021-05-01 23:57:04 +0900

    AlarmManager 구현준비
    AlarmManager 문서 참조해서 작성할 것

---
+++ app/src/main/java/com/jhkim/alarmy/AlarmItemAcitivy.kt
@@ -0,0 +1,14 @@
+package com.jhkim.alarmy
+
+import android.app.AlarmManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlarmItemAcitivy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_item_acitivy)
+
+//        AlarmManager alarmManager = Alarm
+    }
+}
