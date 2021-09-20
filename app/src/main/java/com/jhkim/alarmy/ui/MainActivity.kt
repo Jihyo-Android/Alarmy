a0d7e9473b50b5c1b6f9399035cdd9b509948fac
Jihyo Kim <wlgy33@naver.com>
2021-09-21 01:53:53 +0900

    Widget들 ID 변경, RecyclerView에 사용될 Item의 View만 구현
    Fragment 사용하여 BottomNavigationView 구현 필요

--- app/src/main/java/com/jhkim/alarmy/ui/MainActivity.kt
+++ app/src/main/java/com/jhkim/alarmy/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jhkim.alarmy
+package com.jhkim.alarmy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jhkim.alarmy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
