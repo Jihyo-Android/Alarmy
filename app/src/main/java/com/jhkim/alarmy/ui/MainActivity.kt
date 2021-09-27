bbac389582e9d587586c143dc7431d7e761b32e1
Mymac <wlgy33@naver.com>
2021-09-27 22:04:33 +0900

    Fragment 만드는 중

--- app/src/main/java/com/jhkim/alarmy/ui/MainActivity.kt
+++ app/src/main/java/com/jhkim/alarmy/ui/MainActivity.kt
@@ -3,10 +3,25 @@ package com.jhkim.alarmy.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jhkim.alarmy.R
+import com.jhkim.alarmy.ui.fragment.AlarmRecyclerViewFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setFragment(0)
+    }
+
+    private fun setFragment(fragmentNum: Int) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        when (fragmentNum) {
+            0 -> {
+                fragmentTransaction.replace(R.id.activity_main_fl, AlarmRecyclerViewFragment()).commit()
+            }
+
+
+        }
     }
 }
