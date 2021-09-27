869cec1e27e446974d41ac6d812cfd7e1f90726b
Mymac <wlgy33@naver.com>
2021-09-27 22:10:16 +0900

    Fragment 만드는 중
    Fragment 화면 제작

---
+++ app/src/main/java/com/jhkim/alarmy/ui/fragment/AlarmRecyclerViewFragment.kt
@@ -0,0 +1,19 @@
+package com.jhkim.alarmy.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jhkim.alarmy.R
+
+class AlarmRecyclerViewFragment: Fragment(R.layout.fragment_alarm_recyclerview) {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_alarm_recyclerview, container, false)
+
+
+
+        return view
+    }
+}
